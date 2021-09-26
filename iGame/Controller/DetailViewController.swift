7714fba969a12e065e3c790558cea7919261d77a
setiawanjoddy <37212064+setiawanjoddy@users.noreply.github.com>
2021-09-26 23:48:59 +0700

    DetailView Initial

---
+++ iGame/Controller/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  iGame
+//
+//  Created by Setiawan Joddy on 26/09/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var gamesDetailImageView: UIImageView!
+    @IBOutlet weak var gamesDetailTitleLabel: UILabel!
+    @IBOutlet weak var gamesRDDetailLabel: UILabel!
+    @IBOutlet weak var gamesRatingDetailLabel: UILabel!
+    @IBOutlet weak var gamesDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
